02008f9c6c53ec68259e7c528128d509812f4e2f
Evgeny Svirsky <evgeny@funler.com>
2019-04-25 16:47:04 +0300

    Initial commit

---
+++ android/app/src/main/java/com/widgetexample2/MainApplication.java
@@ -0,0 +1,51 @@
+package com.widgetexample2;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.cere.rewards_module.RewardsModule;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+
+    RewardsModule rewardsModule = new RewardsModule(this);
+    rewardsModule.init("239")
+      .onInitializationFinished(() -> rewardsModule.show("top_section_3"));
+  }
+}
